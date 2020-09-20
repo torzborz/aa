b824ee1b83995bd48038a1ae6ffa0cd1015dcafb
torzborz <torzborz@gmail.com>
2020-09-20 22:40:25 +0200

    photo list style

--- web/src/pages/Photographs.vue
+++ web/src/pages/Photographs.vue
@@ -2,15 +2,31 @@
   <Layout>
 
     <!-- <h1>Photographs</h1> -->
-
-    <div v-for="photo in photos" :key="photo.id">
-      <g-link :to="'/photograph/' + photo.slug">
-        <img :src="photo.src" />
-      </g-link>
+    <div class="flex flex-wrap">
+      <div v-for="photo in photos" :key="photo.id" class="w-1/2 p-1 flex items-center justify-center">
+        <g-link :to="'/photograph/' + photo.slug">
+          <g-image
+            class="m-auto"
+            :alt="photo.alt"
+            :src="$urlForImage(photo.image, $static.metadata.sanityOptions).auto('format').url()"
+          />
+        </g-link>
+      </div>
     </div>
 
   </Layout>
 </template>
+<static-query>
+{
+  metadata {
+    siteName,
+    sanityOptions {
+      projectId
+      dataset
+    }
+  }
+}
+</static-query>
 
 <page-query>
 {
@@ -42,12 +58,15 @@ export default {
     }
   },
   mounted() {
+    let j = 0;
     for (const edge of this.$page.posts.edges) {
-      if (edge.node.images.length>0) {
+      if (edge.node.images.length > 0) {
         const photo = {};
         const i = Math.round(Math.random() * edge.node.images.length);
+        photo.id = j++;
         photo.slug = edge.node.slug.current;
-        photo.src = edge.node.images[i].asset.url;
+        photo.alt = edge.node.title;
+        photo.image = edge.node.images[i];
         this.photos.push(photo);
       }
     }
