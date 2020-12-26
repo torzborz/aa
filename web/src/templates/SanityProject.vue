09b127640d2a0d654aa132c24bdb5315c5af19bd
torzborz <torzborz@gmail.com>
2020-12-26 21:04:47 +0100

    project refinements

--- web/src/templates/SanityProject.vue
+++ web/src/templates/SanityProject.vue
@@ -1,11 +1,44 @@
 <template>
   <Layout>
 
-      <block-content
-        class="project__content"
-        :blocks="$page.project._rawBody"
-        v-if="$page.project._rawBody"
-      />
+    <div class="flex">
+      <dl class="w-1/2">
+        <dt class="text-xs uppercase">project name</dt>
+        <dd>{{ $page.project.title }}</dd>
+
+        <dt class="text-xs uppercase">medium</dt>
+        <dd>{{ $page.project.mediums }}</dd>
+
+        <div class="flex">
+          <div class="w-1/2">
+            <dt class="text-xs uppercase">year</dt>
+            <dd>{{ $page.project.year }}</dd>
+          </div>
+
+          <div class="w-1/2">
+            <dt class="text-xs uppercase">location</dt>
+            <dd>{{ $page.project.location }}</dd>
+          </div>
+        </div>
+      </dl>
+
+      <dl class="w-1/2">
+        <dt class="text-xs uppercase">about</dt>
+        <dd>{{ $page.project.about }}</dd>
+
+        <dt v-if="$page.project.location" class="text-xs uppercase">collaboration with</dt>
+        <dd v-if="$page.project.location">{{ $page.project.location }}</dd>
+
+        <dt v-if="$page.project.download" class="text-xs uppercase">download</dt>
+        <dd v-if="$page.project.download">{{ $page.project.download }}</dd>
+      </dl>
+    </div>
+
+    <block-content
+      class="project__content"
+      :blocks="$page.project._rawBody"
+      v-if="$page.project._rawBody"
+    />
 
   </Layout>
 </template>
